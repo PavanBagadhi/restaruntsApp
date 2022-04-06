a654ebbe37c48dce61378317485ef178f2230902
PavanBagadhi <bagadhipavan1997@gmail.com>
2022-04-06 22:49:09 +0530

    Update SubMenuSectionModal.js
    New branch initial commit

--- src/Components/Menu/SubMenuSectionModal.js
+++ src/Components/Menu/SubMenuSectionModal.js
@@ -3,7 +3,7 @@ import React from 'react';
 import MenuTable from '../UI/MenuTable';
 
 const SubMenuSectionModal = ({ open, handleToClose, menuDetails, sectionName }) => {
-  console.log(menuDetails, "afadfae")
+  console.log(menuDetails, "new  Branch")
   return (
     <Dialog onClose={handleToClose} open={open} fullWidth>
       <DialogTitle>{sectionName}</DialogTitle>
